7c3113a2063e7ba26855110aeb2e9a6dacdab545
ciraganenicole <ciraganenicole@gmail.com>
2022-08-16 16:48:49 +0200

    Add the store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,10 +2,13 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import developerReducer from './reducers/developer';
+import userReducer from './reducers/user';
+import reservationReducer from './reducers/reservation';
 
 const rootReducer = combineReducers({
-
+  user: userReducer,
   developer: developerReducer,
+  reservation: reservationReducer,
 });
 
 const store = createStore(
