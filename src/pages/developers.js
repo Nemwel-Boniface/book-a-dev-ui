708d7e349e1610d9b3b374963cf8f5fee4654fca
maxthestranger <thestrangermax@gmail.com>
2022-08-26 10:22:55 +0300

    add couresel to developer page

--- src/pages/developers.js
+++ src/pages/developers.js
@@ -1,6 +1,12 @@
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
 import Layout from '../layouts/layout';
 import SingleDeveloper from '../components/developer/single_developer';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const Developers = () => {
   const { developers, error } = useSelector((state) => state.developer);
@@ -11,13 +17,44 @@ const Developers = () => {
         <h1 className="title">Developer List</h1>
         <p className="desc">Please select a developer</p>
 
-        <div className="dev_wrapper">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={30}
+          slidesPerGroup={2}
+          loop
+          loopFillGroupWithBlank
+          pagination={{
+            clickable: true,
+          }}
+          navigation
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              slidesPerGroup: 1,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              spaceBetween: 30,
+            },
+            1024: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              spaceBetween: 30,
+            },
+          }}
+        >
           {!error && developers
             ? developers.map((developer) => (
-              <SingleDeveloper developer={developer} key={developer.id} />
+              <SwiperSlide key={developer.id}>
+                <SingleDeveloper developer={developer} />
+              </SwiperSlide>
             ))
             : 'No developers found'}
-        </div>
+        </Swiper>
       </div>
     </Layout>
   );
